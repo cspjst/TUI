12e1cd4242afa16319ce8c2dab90f54bec79776c
Hellorld! <jeremy.thornton@nhs.net>
2025-08-01 11:43:16 +0100

    Update mda_context.h

--- src/MDA/mda_context.h
+++ src/MDA/mda_context.h
@@ -3,20 +3,20 @@
 
 #include <stdbool.h>
 
-void mda_plot(mda_rect_t* rect, uint8_t x, uint8_t y, char c, attribute_t attr);
+void mda_plot(mda_rect_t* rect, uint8_t x, uint8_t y,  mda_char_attr_t chatt);
 
-void mda_vline(mda_rect_t* rect, uint8_t x0, uint8_t y0, , uint8_t x1, uint8_t y1, char c, attribute_t attr);
+void mda_vline(mda_rect_t* rect, uint8_t x0, uint8_t y0, , uint8_t x1, uint8_t y1,  mda_char_attr_t chatt);
 
-void mda_hline(mda_rect_t* rect, uint8_t x0, uint8_t y0, , uint8_t x1, uint8_t y1, char c, attribute_t attr);
+void mda_hline(mda_rect_t* rect, uint8_t x0, uint8_t y0, , uint8_t x1, uint8_t y1,  mda_char_attr_t chatt);
 
-void mda_box(mda_rect_t* rect, mda_rect_t* box, , char c, attribute_t attr);
+void mda_box(mda_rect_t* rect, mda_rect_t* box, , mda_char_attr_t chatt);
 
-void mda_box_fill(mda_rect_t* rect, mda_rect_t* box, , char c, attribute_t attr);
+void mda_box_fill(mda_rect_t* rect, mda_rect_t* box, , mda_char_attr_t chatt);
 
 void mda_blit(mda_rect_t* rect_dst, mda_rect* rect_src);
 
 bool mda_contains(mda_rect_t* rect, uint8_t x, uint8_t y);
 
-bool mda_intersect((mda_rect_t* rect_a, mda_rect* rect_b);
+bool mda_intersect(mda_rect_t* rect_a, mda_rect* rect_b);
 
 #endif
