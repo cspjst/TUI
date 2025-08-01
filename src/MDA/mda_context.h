78669f988ba6b1085aaf1e59cef47158a560075e
Hellorld! <jeremy.thornton@nhs.net>
2025-08-01 13:30:53 +0100

    Update mda_context.h

--- src/MDA/mda_context.h
+++ src/MDA/mda_context.h
@@ -15,6 +15,10 @@ typedef struct {
     // TODO: mouse_state mouse; has mouse support etc
 } mda_context_t;
 
+void mda_set_context_frame(mda_context_t* ctx, uint8_t x, uint8_t y, uint8_t width, uint8_t height);
+
+void mda_initialize_default_context(mda_context_t* ctx);
+
 void mda_plot(mda_rect_t* rect, uint8_t x, uint8_t y,  mda_char_attr_t chatt);
 
 void mda_vline(mda_rect_t* rect, uint8_t x0, uint8_t y0, , uint8_t x1, uint8_t y1,  mda_char_attr_t chatt);
@@ -30,5 +34,31 @@ void mda_blit(mda_rect_t* rect_dst, mda_rect* rect_src);
 bool mda_contains(mda_rect_t* rect, uint8_t x, uint8_t y);
 
 bool mda_intersect(mda_rect_t* rect_a, mda_rect* rect_b);
+
+void mda_cursor_to(mda_context_t* ctx, uint8_t x, uint8_t y);
+
+void mda_cursor_advance(mda_context_t* ctx);
+
+void mda_ascii_BEL(mda_context_t* ctx);
+
+void mda_ascii_BS(mda_context_t* ctx);
+
+void mda_ascii_HT(mda_context_t* ctx);
+
+void mda_ascii_LF(mda_context_t* ctx);
+
+void mda_ascii_VT(mda_context_t* ctx);
+
+void mda_ascii_FF(mda_context_t* ctx);
+
+void mda_ascii_CR(mda_context_t* ctx);
+
+void mda_ascii_ESC(mda_context_t* ctx);
+
+void mda_ascii_DEL(mda_context_t* ctx);
+
+void mda_write_CRLF(mda_context_t* ctx);
+
+void mda_write_char(mda_context_t* ctx, char chr);
 
 #endif
