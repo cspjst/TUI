192d4de297a6106d60d02423dd61423fcb4b3bcf
Hellorld! <jeremy.thornton@nhs.net>
2025-08-01 13:51:40 +0100

    Update mda_context.c

--- src/MDA/mda_context.c
+++ src/MDA/mda_context.c
@@ -1,2 +1,22 @@
 #include mda_context.h
 
+#include ../CONTRACT/contract.h
+
+void mda_set_bounds(mda_context_t* ctx, uint8_t x, uint8_t y, uint8_t w, uint8_t h) {
+  ctx->bounds.rect.x = x;
+  ctx->bounds.rect.y = y;
+  ctx->bounds.rect.w = w;
+  ctx->bounds.rect.h = h;
+}
+
+void mda_initialize_default_context(mda_context_t* ctx) {
+    require_addr(ctx, "NULL context!");
+    bios_set_video_mode(MDA_TEXT_MONOCHROME_80X25);
+    bios_get_video_state(&ctx->video);
+    bios_get_cursor_position_and_size(&ctx->cursor, ctx->video.page);
+    mda_set_bounds(ctx, 0, 0, ctx->video.columns, MDA_SCREEN_ROWS);
+    ctx->attributes = MDA_NORMAL;
+    ctx->htab_size = MDA_DEFAULT_HTAB;
+    ctx->vtab_size = MDA_DEFAULT_VTAB;
+}
+
