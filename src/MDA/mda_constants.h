91d0ea252d89dff543ae66deefb12e44731c4b9e
Hellorld! <jeremy.thornton@nhs.net>
2025-08-01 17:29:01 +0100

    Update mda_constants.h

--- src/MDA/mda_constants.h
+++ src/MDA/mda_constants.h
@@ -1,7 +1,9 @@
 #ifndef MDA_CONSTANTS_H
 #define MDA_CONSTANTS_H
 
-#define MDA_SCREEN_ROWS     25
+#define MDA_SEGMENT        0B000h
+#define MDA_COLUMNS        80
+#define MDA_ROWS           25
 #define MDA_DEFAULT_HTAB    4
 #define MDA_DEFAULT_VTAB    2
 
