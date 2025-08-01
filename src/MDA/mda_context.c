df08ca0de32fae3a20bfd45bc715fd02a3d7b766
Hellorld! <jeremy.thornton@nhs.net>
2025-08-01 17:50:30 +0100

    Update mda_context.c

--- src/MDA/mda_context.c
+++ src/MDA/mda_context.c
@@ -1,6 +1,6 @@
 #include mda_context.h
-
-#include ../CONTRACT/contract.h
+#include "../CONTRACT/contract.h"
+#include "../BIOS/bios_video_services.h"
 
 void mda_set_bounds(mda_context_t* ctx, uint8_t x, uint8_t y, uint8_t w, uint8_t h) {
   ctx->bounds.rect.x = x;
@@ -20,3 +20,25 @@ void mda_initialize_default_context(mda_context_t* ctx) {
     ctx->vtab_size = MDA_DEFAULT_VTAB;
 }
 
+void mda_plot(mda_rect_t* rect, uint16_t x, uint16_t y, mda_cell_t cell) {
+    require_addr(ctx, "NULL context!");
+    __asm {
+        .8086
+        // 1. register setup
+        mov ax, MDA_SEGMENT
+        mov es, ax
+        mov di, y
+        // 2. DI = y * 80
+        mov  ax, di      
+        shl  ax, 2       ; y * 4
+        add  ax, di      ; y * 5
+        shl  ax, 4       ; y * 5 * 16
+        add  ax, x       ; ax = y*80 + x
+        shl  ax, 1       ; word offset
+        mov  di, ax    
+        mov  ax, cell 
+        cld
+        stosw
+        
+    }
+}
