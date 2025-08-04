a3abad09a5fad953a89518ce60160e747e3f87e3
Hellorld! <jeremy.thornton@nhs.net>
2025-08-04 11:01:24 +0100

    Update mda_context.c

--- src/MDA/mda_context.c
+++ src/MDA/mda_context.c
@@ -25,28 +25,26 @@ void mda_plot(mda_point_t* point, mda_cell_t* cell) {
         .8086
         // 1. register setup
         mov ax, MDA_SEGMENT
-        mov es, ax      ; ES:DI *VRAM
-        lds si, point   ; DS:SI *point
-        lodsb           ; AL = x
-        mov bl, al      ; BL = x
-        sub bh, bh      ; BX = x
-        lodsb           ; AL = y
-        sub ah, ah      ; AX = y
-        mov di, ax      ; DI copy y
+        mov es, ax          ; ES:DI *VRAM
+        lds si, point       ; DS:SI *point
+        mov bl, ds:[di]     ; BL = x
+        sub bh, bh          ; BX = x
+         mov al, ds:[di+1]  ; AL = y
+        sub ah, ah          ; AX = y
+        mov di, ax          ; DI copy y
         // 2. DI = y * 80
-        shl  ax, 1       ; y * 4
+        shl  ax, 1           ; y * 4
         shl  ax, 1
-        add  ax, di      ; y * 5
-        shl  ax, 1       ; y * 5 * 16
+        add  ax, di          ; y * 5
+        shl  ax, 1           ; y * 5 * 16
         shl  ax, 1
         shl  ax, 1
         shl  ax, 1
-        add  ax, bx      ; ax = y*80 + x
-        shl  ax, 1       ; word offset
-        mov  di, ax      ; ES:DI *VRAM (x,y)
-        lds  si, cell    ; DS:SI *cell
-        //cld            ; direction flag irrelevant single MOV
-        movsw            ; *VRAM = *cell
+        add  ax, bx          ; ax = y*80 + x
+        shl  ax, 1           ; word offset
+        mov  di, ax          ; ES:DI *VRAM (x,y)
+        lds  si, cell        ; DS:SI *cell
+        movsw                ; *VRAM = *cell
     }
 }
 
@@ -62,15 +60,16 @@ void mda_hline((mda_point_t* p0, mda_point_t* p1, mda_cell_t* cell) {
         mov al, ds:[di+1]   ; AL = p0.y
         sub ah, ah          ; AX = p0.y
         mov di, ax          ; DI copy p0.y
-        lds si, p0          ; DS:SI *p0
-        mov cl, ds:[di+1]   ; CL = p1.y
-        sub ch, ch          ; CX = p1.y
-        cmp cl, bl          ; y1 > y0 ?
+        lds si, p1          ; DS:SI *p1
+        mov cl, ds:[di]     ; CL = p1.x
+        sub ch, ch          ; CX = p1.x
+        // 2. check valid points
+        cmp cl, al          ; x1 > x0 ?
         jg OK
-        xchg cl, bl         ; swap y1 y0
-OK:     sub cl, bl          ; CL = distance y0..y1
+        xchg cl, al         ; swap y1 y0
+OK:     sub cl, al          ; CL = distance x0..x1
         inc cl              ; CL = line width
-        // 2. DI = y * 80
+        // 3. DI = y * 80
         shl  ax, 1       ; y * 4
         shl  ax, 1
         add  ax, di      ; y * 5
@@ -93,21 +92,24 @@ void mda_vline((mda_point_t* p0, mda_point_t* p1, mda_cell_t* cell) {
         .8086
         // 1. register setup
         mov ax, MDA_SEGMENT
-        mov es, ax
-        mov ax, p0
-        mov bl, ah      ; BL = p0.x
-        sub bh, bh      ; BX = p0.x
-        sub ah, ah      ; AX = p0.y
-        mov di, ax
-        mov dx, MDA_ROW_BYTES   ; 160
-        mov cx, p1
-        sub ch, ch      ; CX = p1.y
-        cmp cl, al
+        mov es, ax          ; ES:DI *VRAM
+        lds si, p0          ; DS:SI *p0
+        mov bl, ds:[di]     ; BL = p0.x
+        sub bh, bh          ; BX = p0.x
+        mov al, ds:[di+1]   ; AL = p0.y
+        sub ah, ah          ; AX = p0.y
+        mov di, ax          ; DI copy p0.y
+        lds si, p1          ; DS:SI *p1
+        mov cl, ds:[di+1]   ; CL = p1.y
+        sub ch, ch          ; CX = p1.y
+        mov dx, MDA_ROW_BYTES ; DX = 160
+        // 2. check valid points
+        cmp cl, bl          ; y1 > y0 ?
         jg OK
-        xchg cl, al
-OK:     sub cl, al
-        inc cl
-        // 2. DI = y * 80
+        xchg cl, bl         ; swap y1 y0
+OK:     sub cl, bl          ; CL = distance y0..y1
+        inc cl              ; CL = line height
+        // 3. DI = y * 80
         shl  ax, 1       ; y * 4
         shl  ax, 1
         add  ax, di      ; y * 5
@@ -118,11 +120,11 @@ OK:     sub cl, al
         add  ax, bx      ; ax = y*80 + x
         shl  ax, 1       ; word offset
         mov  di, ax
-        mov  ax, cell
-        cld
+        lds  si, cell    ; DS:SI *cell
+        lodsw            ; AX = cell
         // 3. vertical plots
 NEXT:   mov es:[di], ax
-        add di, dx
+        add di, dx       ; add reg, reg is faster than add reg, imm 
         loop NEXT
     }
 }
