78df6e7d2172cc3a0d37ffc53f0ff19f36411b34
Hellorld! <jeremy.thornton@nhs.net>
2025-08-05 19:10:02 +0100

    Create mda_dim.h

---
+++ src/MDA/mda_dim.h
@@ -0,0 +1,26 @@
+#ifndef MDA_DIM_H
+#define MDA_DIM_H
+
+#include "../CONTRACT/contract.h"
+#include <stdint.h>
+
+typedef union {
+    uint16_t packed;
+    struct {
+        uint8_t h, w;
+    } dim;
+} mda_dim_t;
+
+static inline void mda_dim_init(mda_dim_t* d, uint8_t w, uint8_t h) {
+    require_address(d, "NULL dimension!");
+    d->dim.w = w;
+    d->dim.h = h;
+}
+
+static inline mda_dim_t mda_dim_make(uint8_t w, uint8_t h) {
+    mda_dim_t d;
+    mda_dim_init(&d, w, h);
+    return d;
+}
+
+#endif
