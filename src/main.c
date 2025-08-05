91f475929e4a8571eb1676a4683af1c3434874cd
Hellorld! <jeremy.thornton@nhs.net>
2025-08-05 10:15:09 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 #include "MDA/mda_context.h"
-#include "MDA/mda_cell.h"
-#include "MDA/mda_point.h"
+#include "MDA/demo_tui.h
 
 int main() {
     printf("TUI - mda_cell_t Interface Exercise\n");
@@ -10,67 +9,9 @@ int main() {
     mda_context_t ctx;
     mda_initialize_default_context(&ctx);
 
-    mda_cell_t cell = mda_cell_make('*', ctx.attributes);
-    mda_point_t p0 = mda_point_make(5, 2);
-    mda_point_t p1 = mda_point_make(20, 10);
-    print_point(p0);
-    print_point(p1);
-    mda_plot(p0.packed, cell.packed);
-    mda_plot(p1.packed, cell.packed);
+    demo_plot(&ctx);
 
     getchar();
 
     return 0;
 }
-
-
-/*
-for(int i = 0; i < 20; ++i) {
-    mda_hline(p0.packed,p1.packed, cell.packed);
-    p0.pos.y++;
-    p1.pos.x++;
-}
-
-for(int i = 0; i < 20; ++i) {
-    mda_vline(p0.packed,p1.packed, cell.packed);
-    p0.pos.x += 2;
-    p1.pos.y++;
-}
-*/
-
-/*
-// 1. Initialize in place
-mda_init_cell(&cell, 'A', ctx.attributes);
-mda_plot(p.packed, cell.packed);
-
-// 2. Modify attribute: OR in bold
-mda_cell_bold(&cell);
-p.pos.x++;
-mda_plot(p.packed, cell.packed);
-
-// 3. Set new character
-mda_cell_set_char(&cell, 'B');
-p.pos.x++;
-mda_plot(p.packed, cell.packed);
-
-// 4. Set underline (replaces current attr)
-mda_cell_set_attr(&cell, MDA_UNDERLINE);
-p.pos.x++;
-mda_plot(p.packed, cell.packed);
-
-// 5. OR in blink
-mda_cell_blink(&cell);
-p.pos.x++;
-mda_plot(p.packed, cell.packed);
-
-// 6. Use make_cell for temporary
-cell = mda_make_cell('C', MDA_REVERSE);
-p.pos.x++;
-mda_plot(p.packed, cell.packed);
-
-// 7. Direct packed access
-mda_cell_set_char(&cell, 'D');
-mda_cell_set_attr(&cell, MDA_BOLD | MDA_BLINK);
-p.pos.x++;
-mda_plot(p.packed, cell.packed);
- */
