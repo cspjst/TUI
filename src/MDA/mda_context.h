7da2c21bc280f5250f1dc9c5fbb3067640e75267
Hellorld! <jeremy.thornton@nhs.net>
2025-08-07 11:05:35 +0100

    Update mda_context.h

--- src/MDA/mda_context.h
+++ src/MDA/mda_context.h
@@ -34,22 +34,22 @@ typedef struct {
 
 void mda_plot(mda_point_t* point, mda_cell_t* cell);
 
-void mda_hline(mda_point_t* p0, mda_point_t* p1, mda_cell_t* cell);
+void mda_draw_hline(mda_point_t* p0, mda_point_t* p1, mda_cell_t* cell);
 
-void mda_vline(mda_point_t* p0, mda_point_t* p1, mda_cell_t* cell);
+void mda_draw_vline(mda_point_t* p0, mda_point_t* p1, mda_cell_t* cell);
 
 // (3 cells) = lhs end cap, hline, rhs end cap
-void mda_hline_caps(mda_point_t* point, mda_cell_t* cells);
+void mda_draw_hline_caps(mda_point_t* point, mda_cell_t* cells);
 
 // (3 cells) - top end cap, vline, bottom end cap
-void mda_vline_caps(mda_point_t* point, mda_cell_t* cells);
+void mda_draw_vline_caps(mda_point_t* point, mda_cell_t* cells);
 
 void mda_draw_rect(mda_rect_t* rect, mda_cell_t* cell);
 
 void mda_fill_rect(mda_rect_t* rect, mda_cell_t* cell);
 
 // (8 cells) = top-left,top-hline,top-right,vline-left,vline-right,bottom-left,bottom-hline,bottom-right
-void mda_border(mda_rect_t* rect, mda_cell_t* cells);
+void mda_draw_border(mda_rect_t* rect, mda_cell_t* cells);
 
 void mda_blit(mda_rect_t* to, mda_rect_t* from);
 
